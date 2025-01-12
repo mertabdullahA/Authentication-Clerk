7a4bd9f4f224a49e67944aa261a3411c5f37425a
Abdullah Mert ÇANKAYA <mertcanmert1515@gmail.com>
2025-01-12 16:31:40 +0300

    Add files via upload

---
+++ app/components/Heaader.jsx
@@ -0,0 +1,47 @@
+"use client";
+import Link from "next/link";
+import { UserButton, useAuth } from "@clerk/nextjs";
+
+const Header = () => {
+  const { userId } = useAuth();
+
+  return (
+    <nav className="flex items-center justify-between px-6 py-4 mb-5 bg-blue-700">
+      <div className="flex items-center">
+        <Link href="/">
+          <div className="text-lg font-bold text-white uppercase">
+            Clerk App
+          </div>
+        </Link>
+      </div>
+      <div className="flex items-center text-white">
+        {!userId && (
+          <>
+            <Link
+              href="/sign-in"
+              className="text-gray-300 hover:text-white mr-4"
+            >
+              Giriş Yap
+            </Link>
+            <Link
+              href="/sign-up"
+              className="text-gray-300 hover:text-white mr-4"
+            >
+              Kayıt Ol{" "}
+            </Link>
+          </>
+        )}
+        {userId && (
+          <Link href="/profile" className="text-gray-300 hover:text-white mr-4">
+            Profile
+          </Link>
+        )}
+        <div className="ml-auto">
+          <UserButton afterSignOutUrl="/" />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
---
+++ app/dashboard/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-5">Yönetim Paneli</h1>
+      <p className="mb-5">Yönetim Paneline hoşgeldiniz </p>
+    </div>
+  );
+};
+
+export default DashboardPage;
